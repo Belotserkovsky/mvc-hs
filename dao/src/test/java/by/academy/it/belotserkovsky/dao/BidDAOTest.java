925c27a756c3dfbbd053b40dddcc583f7fc99316
Belotserkovsky <bkostby@gmail.com>
2016-04-22 19:56:59 +0300

    commit

--- dao/src/test/java/by/academy/it/belotserkovsky/dao/BidDAOTest.java
+++ dao/src/test/java/by/academy/it/belotserkovsky/dao/BidDAOTest.java
@@ -1,8 +1,13 @@
 package by.academy.it.belotserkovsky.dao;
 
 import by.academy.it.belotserkovsky.entity.Bid;
+import by.academy.it.belotserkovsky.poolConnection.DataSource;
+import org.junit.After;
+import org.junit.Assert;
 import org.junit.Test;
 
+import java.sql.Connection;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,6 +18,8 @@ import static org.junit.Assert.*;
  *
  */
 public class BidDAOTest {
+    private BidDAO bidDAO;
+
     @Test
     public void readAll() throws Exception {
 
@@ -24,4 +31,37 @@ public class BidDAOTest {
 
     }
 
+    @Test
+    public void create() throws Exception {
+        Bid bid = new Bid();
+
+        bid.setKindOfWorks("kind of works");
+        bid.setScope("scope");
+        bid.setDesiredRuntime("desired runtime");
+        bid.setUserId(1);
+        bid.setBrigadeId(1);
+        bidDAO = BidDAO.getInstance();
+        bidDAO.create(bid);
+
+        List<Bid> bidList = BidDAO.getInstance().readAll();
+
+        //Assert.assertEquals(1, bidList.size());
+        Assert.assertEquals(bid.getDesiredRuntime(), bidList.get(0).getDesiredRuntime());
+
+    }
+
+    @After
+    public void deleteAll() throws Exception{
+        Connection connection = null;
+        Statement statement = null;
+        String query = "TRUNCATE TABLE bids";
+
+        connection = DataSource.getInstance().getConnection();
+        statement = connection.createStatement();
+
+        statement.execute(query);
+
+        statement.close();
+        connection.close();
+    }
 }
